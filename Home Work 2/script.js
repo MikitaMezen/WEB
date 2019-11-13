1688446e424d15ad4a9893b6c1a1d1e5c1631810
Nikita Mezentsew <57619961+MikitaMezen@users.noreply.github.com>
2019-11-13 23:07:35 +0300

    Task 2

--- Home Work 2/script.js
+++ Home Work 2/script.js
@@ -1,1 +1,17 @@
+  
+function goToGoogle(){
+    document.location.href = "http://www.google.com";
+}
+function ClearAll(){
+    document.getElementById("Username").innerHTML = "";
+}
+
+var user = prompt("Enter username : ");
+
+if(user == null || user == "")
+    document.getElementById("Username").innerHTML += "guest";
+else if(/[0-9]/.test(user))
+    document.getElementById("Username").innerHTML += user.split("").reverse().join("");
+else
+    document.getElementById("Username").innerHTML += user.split("").map((v,i) => i % 2 == 0 ? v.toUpperCase() : v.toLowerCase()).join("");
 
